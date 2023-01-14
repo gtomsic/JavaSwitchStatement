8e91459ff7ed558880bfc3945c296ccc933196c9
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-13 22:51:56 -0700

    Print number complete

---
+++ src/PrintNumber.java
@@ -0,0 +1,22 @@
+public class PrintNumber {
+    public static void main(String[] args) {
+    printNumberInWord(-1);
+    }
+
+    public static void printNumberInWord(int num) {
+        String number = switch (num) {
+            case 0 -> "ZERO";
+            case 1 -> "ONE";
+            case 2 -> "TWO";
+            case 3 -> "THREE";
+            case 4 -> "FOUR";
+            case 5 -> "FIVE";
+            case 6 -> "SIX";
+            case 7 -> "SEVEN";
+            case 8 -> "EIGHT";
+            case 9 -> "NINE";
+            default -> "OTHER";
+        };
+        System.out.println(number);
+    }
+}
