7beccd1907849636fa65b50508ce1cd2462d8f32
Gabriel Tomsic <tomsicgabriel@gmail.com>
2023-01-13 21:49:04 -0700

    Old switch statement cases

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,30 @@ public class Main {
             }
             default -> System.out.println("No match found.");
         }
+
+        String month = "OCTOBER";
+        System.out.println(month + " is in the " + getQuarter(month) + " quarter");
+    }
+
+    public static String getQuarter(String month) {
+        switch (month){
+            case "JANUARY":
+            case "FEBRUARY":
+            case "MARCH":
+                return "1st";
+            case "APRIL":
+            case "MAY":
+            case "JUNE":
+                return "2nd";
+            case "JULY":
+            case "AUGUST":
+            case "SEPTEMBER":
+                return "3rd";
+            case "OCTOBER":
+            case "NOVEMBER":
+            case "DECEMBER":
+                return "4th";
+        }
+        return "bad";
     }
 }
